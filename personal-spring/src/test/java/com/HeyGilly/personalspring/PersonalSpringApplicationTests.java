9c33e1ba51948a8a88442b7d006f945227d58d83
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-05-16 21:06:41 -0500

    Initial commit

---
+++ personal-spring/src/test/java/com/HeyGilly/personalspring/PersonalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HeyGilly.personalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
