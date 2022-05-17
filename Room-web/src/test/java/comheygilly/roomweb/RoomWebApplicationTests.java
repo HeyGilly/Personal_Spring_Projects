29a72152069f3192e10b1b786dd4c4d95ff0b32d
Nicholas Gilmore <Nick.gilmore20@gmail.com>
2022-05-17 00:04:29 -0500

    Initial Commit

--- Room-web/src/test/java/comheygilly/roomweb/RoomWebApplicationTests.java
+++ Room-web/src/test/java/comheygilly/roomweb/RoomWebApplicationTests.java
@@ -1,13 +1,13 @@
-package com.HeyGilly.personalspring;
+package comheygilly.roomweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PersonalSpringApplicationTests {
+class RoomWebApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
